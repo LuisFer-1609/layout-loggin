ceb96aa12e8091fcc3d043a13f025f807f8ff1ef
castaneda <castaneda@MacBook-Air-de-castaneda.local>
2024-04-14 03:27:51 -0600

    estructura de app

---
+++ templates/navbar_left.php
@@ -0,0 +1,244 @@
+
+      <!--Sidenav Izquierda -->
+      <div class="sidebar-end p-4 bg-mode custom-scrollbar h-100 os-host os-theme-dark os-host-overflow os-host-overflow-y os-host-resize-disabled os-host-scrollbar-horizontal-hidden os-host-transition"><div class="os-resize-observer-host observed"><div class="os-resize-observer" style="left: 0px; right: auto;"></div></div><div class="os-size-auto-observer observed" style="height: calc(100% + 1px); float: left;"><div class="os-resize-observer"></div></div><div class="os-content-glue" style="margin: -24px; width: 79px; height: 676px;"></div><div class="os-padding"><div class="os-viewport os-viewport-native-scrollbars-invisible" style="overflow-y: scroll;"><div class="os-content" style="padding: 24px; height: 100%; width: 100%;">
+
+<div class="sidebar-end-alignment d-flex justify-content-center flex-column">
+
+ <!-- Right sidebar toggle -->
+ <div class="d-flex gap-2 align-items-center">
+  <a class="btn p-0 text-secondary sidebar-end-toggle d-none d-lg-flex">
+    <i class="bi bi-justify-left fs-3"></i>
+  </a>
+  <h5 class="contact-title mb-0">Contacts</h5>
+</div>
+
+  <!-- Search contact START -->
+  <form class="contact-search rounded position-relative">
+    <input class="form-control bg-light" type="search" placeholder="Search..." aria-label="Search">
+  </form>
+  <!-- Search contact END -->
+  <ul class="list-unstyled">
+
+    <!-- Contact item -->
+    <li class="mt-3 hstack gap-3 align-items-center position-relative toast-btn" data-target="chatToast">
+      <!-- Avatar -->
+      <div class="avatar avatar-xs">
+        <a href="#"><img class="avatar-img rounded-circle" src="assets/images/avatar/01.jpg" alt=""></a>
+      </div>
+      <!-- Info -->
+      <div class="overflow-hidden contact-name">
+        <a class="h6 mb-0" href="#!">Frances Guerrero </a>
+      </div>
+      <!-- Chat time -->
+      <div class="contact-status ms-auto fs-3"> <i class="bi bi-dot text-success"></i> </div>
+    </li>
+
+    <!-- Contact item -->
+    <li class="mt-3 hstack gap-3 align-items-center position-relative toast-btn" data-target="chatToast2">
+      <!-- Avatar -->
+      <div class="avatar avatar-xs">
+        <a href="#"><img class="avatar-img rounded-circle" src="assets/images/avatar/02.jpg" alt=""></a>
+      </div>
+      <!-- Info -->
+      <div class="overflow-hidden contact-name">
+        <a class="h6 mb-0" href="#!">Lori Ferguson </a>
+      </div>
+      <!-- Chat time -->
+      <div class="contact-status ms-auto fs-3"> <i class="bi bi-dot text-success"></i> </div>
+    </li>
+
+    <!-- Contact item -->
+    <li class="mt-3 hstack gap-3 align-items-center position-relative">
+      <!-- Avatar -->
+      <div class="avatar avatar-xs">
+        <a href="#"><img class="avatar-img rounded-circle" src="assets/images/avatar/placeholder.jpg" alt=""></a>
+      </div>
+      <!-- Info -->
+      <div class="overflow-hidden contact-name">
+        <a class="h6 mb-0" href="#!">Samuel Bishop </a>
+      </div>
+      <!-- Chat time -->
+      <div class="contact-status ms-auto fs-3"> <i class="bi bi-dot text-danger"></i> </div>
+    </li>
+
+    <!-- Contact item -->
+    <li class="mt-3 hstack gap-3 align-items-center position-relative">
+      <!-- Avatar -->
+      <div class="avatar avatar-xs">
+        <a href="#"><img class="avatar-img rounded-circle" src="assets/images/avatar/09.jpg" alt=""></a>
+      </div>
+      <!-- Info -->
+      <div class="overflow-hidden contact-name">
+        <a class="h6 mb-0" href="#!">Louis Crawford </a>
+      </div>
+      <!-- Chat time -->
+      <div class="contact-status small ms-auto text-nowrap"> 20sec </div>
+    </li>
+
+    <!-- Contact item -->
+    <li class="mt-3 hstack gap-3 align-items-center position-relative">
+      <!-- Avatar -->
+      <div class="avatar avatar-xs">
+        <a href="#"><img class="avatar-img rounded-circle" src="assets/images/avatar/04.jpg" alt=""></a>
+      </div>
+      <!-- Info -->
+      <div class="overflow-hidden contact-name">
+        <a class="h6 mb-0" href="#!">Dennis Barrett </a>
+      </div>
+      <!-- Chat time -->
+      <div class="contact-status ms-auto fs-3"> <i class="bi bi-dot text-success"></i> </div>
+    </li>
+
+    <!-- Contact item -->
+    <li class="mt-3 hstack gap-3 align-items-center position-relative">
+      <!-- Avatar -->
+      <div class="avatar avatar-story avatar-xs">
+        <a href="#"><img class="avatar-img rounded-circle" src="assets/images/avatar/05.jpg" alt=""></a>
+      </div>
+      <!-- Info -->
+      <div class="overflow-hidden contact-name">
+        <a class="h6 mb-0" href="#!">Judy Nguyen </a>
+      </div>
+      <!-- Chat time -->
+      <div class="contact-status ms-auto fs-3"> <i class="bi bi-dot text-success"></i> </div>
+    </li>
+
+    <!-- Contact item -->
+    <li class="mt-3 hstack gap-3 align-items-center position-relative">
+      <!-- Avatar -->
+      <div class="avatar avatar-xs">
+        <a href="#"><img class="avatar-img rounded-circle" src="assets/images/avatar/08.jpg" alt=""></a>
+      </div>
+      <!-- Info -->
+      <div class="overflow-hidden contact-name">
+        <a class="h6 mb-0" href="#!">Bryan Knight </a>
+      </div>
+      <!-- Chat time -->
+      <div class="contact-status ms-auto fs-3"> <i class="bi bi-dot text-danger"></i> </div>
+    </li>
+
+    <!-- Contact item -->
+    <li class="mt-3 hstack gap-3 align-items-center position-relative">
+      <!-- Avatar -->
+      <div class="avatar avatar-xs">
+        <a href="#"><img class="avatar-img rounded-circle" src="assets/images/avatar/10.jpg" alt=""></a>
+      </div>
+      <!-- Info -->
+      <div class="overflow-hidden contact-name">
+        <a class="h6 mb-0" href="#!">Jacqueline Miller </a>
+      </div>
+      <!-- Chat time -->
+      <div class="contact-status small ms-auto text-nowrap"> 1min </div>
+    </li>
+
+    <!-- Contact item -->
+    <li class="mt-3 hstack gap-3 align-items-center position-relative">
+      <!-- Avatar -->
+      <div class="avatar avatar-xs">
+        <a href="#"><img class="avatar-img rounded-circle" src="assets/images/avatar/11.jpg" alt=""></a>
+      </div>
+      <!-- Info -->
+      <div class="overflow-hidden contact-name">
+        <a class="h6 mb-0" href="#!">Amanda Reed </a>
+      </div>
+      <!-- Chat time -->
+      <div class="contact-status small ms-auto text-nowrap"> 3min </div>
+    </li>
+
+    <!-- Contact item -->
+    <li class="mt-3 hstack gap-3 align-items-center position-relative">
+      <!-- Avatar -->
+      <div class="avatar avatar-xs">
+        <a href="#"><img class="avatar-img rounded-circle" src="assets/images/avatar/12.jpg" alt=""></a>
+      </div>
+      <!-- Info -->
+      <div class="overflow-hidden contact-name">
+        <a class="h6 mb-0" href="#!">Larry Lawson </a>
+      </div>
+      <!-- Chat time -->
+      <div class="contact-status small ms-auto text-nowrap"> 1hrs </div>
+    </li>
+  </ul>
+  
+  <!-- Groups START -->
+  <h5 class="mt-3 contact-title">Groups</h5>
+
+  <ul class="list-unstyled pb-5">
+
+    <!-- Contact item -->
+    <li class="mt-3 hstack gap-3 align-items-center position-relative">
+      <!-- Avatar -->
+      <div class="flex-shrink-0 avatar">
+        <ul class="avatar-group avatar-group-four">
+          <li class="avatar avatar-xxs">
+            <a href="#"><img class="avatar-img rounded-circle" src="assets/images/avatar/01.jpg" alt="avatar"></a>
+          </li>
+          <li class="avatar avatar-xxs">
+            <a href="#"><img class="avatar-img rounded-circle" src="assets/images/avatar/02.jpg" alt="avatar"></a>
+          </li>
+          <li class="avatar avatar-xxs">
+            <a href="#"><img class="avatar-img rounded-circle" src="assets/images/avatar/03.jpg" alt="avatar"></a>
+          </li>
+          <li class="avatar avatar-xxs">
+            <a href="#"><img class="avatar-img rounded-circle" src="assets/images/avatar/placeholder.jpg" alt="avatar"></a>
+          </li>
+        </ul>
+      </div>
+      <!-- Info -->
+      <div class="overflow-hidden contact-name">
+        <a class="h6 mb-0 stretched-link text-truncate d-inline-block" href="#!">Frances, Lori, Amanda, Lawson </a>
+      </div>
+      <!-- Chat time -->
+      <div class="contact-status ms-auto fs-3"> <i class="bi bi-dot text-success"></i> </div>
+    </li>
+
+    <!-- Contact item -->
+    <li class="mt-3 hstack gap-3 align-items-center position-relative">
+      <!-- Avatar -->
+      <div class="flex-shrink-0 avatar">
+        <ul class="avatar-group avatar-group-two">
+          <li class="avatar avatar-xs">
+            <a href="#"><img class="avatar-img rounded-circle" src="assets/images/avatar/08.jpg" alt="avatar"></a>
+          </li>
+          <li class="avatar avatar-xs">
+            <a href="#"><img class="avatar-img rounded-circle" src="assets/images/avatar/10.jpg" alt="avatar"></a>
+          </li>
+        </ul>
+      </div>
+      <!-- Info -->
+      <div class="overflow-hidden contact-name">
+        <a class="h6 mb-0 stretched-link text-truncate d-inline-block" href="#!">Lawson, Knight </a>
+      </div>
+      <!-- Chat time -->
+      <div class="contact-status ms-auto fs-3"> <i class="bi bi-dot text-danger"></i> </div>
+    </li>
+
+    <!-- Contact item -->
+    <li class="mt-3 hstack gap-3 align-items-center position-relative">
+      <!-- Avatar -->
+      <!-- Avatar -->
+      <div class="flex-shrink-0 avatar">
+        <ul class="avatar-group avatar-group-three">
+          <li class="avatar avatar-xs">
+            <a href="#"><img class="avatar-img rounded-circle" src="assets/images/avatar/12.jpg" alt="avatar"></a>
+          </li>
+          <li class="avatar avatar-xs">
+            <a href="#"><img class="avatar-img rounded-circle" src="assets/images/avatar/11.jpg" alt="avatar"></a>
+          </li>
+          <li class="avatar avatar-xs">
+            <a href="#"><img class="avatar-img rounded-circle" src="assets/images/avatar/04.jpg" alt="avatar"></a>
+          </li>
+        </ul>
+      </div>
+      <!-- Info -->
+      <div class="overflow-hidden contact-name">
+        <a class="h6 mb-0 stretched-link text-truncate d-inline-block" href="#!">Miller, Joan, Vasquez, Ortiz </a>
+      </div>
+      <!-- Chat time -->
+      <div class="contact-status small ms-auto text-nowrap"> 1min </div>
+    </li>
+  </ul>
+</div>
+<!-- Offcanvas body END -->
+</div></div></div><div class="os-scrollbar os-scrollbar-horizontal os-scrollbar-unusable os-scrollbar-auto-hidden"><div class="os-scrollbar-track os-scrollbar-track-off"><div class="os-scrollbar-handle" style="width: 100%; transform: translate(0px, 0px);"></div></div></div><div class="os-scrollbar os-scrollbar-vertical os-scrollbar-auto-hidden"><div class="os-scrollbar-track os-scrollbar-track-off"><div class="os-scrollbar-handle" style="height: 86.5407%; transform: translate(0px, 0px);"></div></div></div><div class="os-scrollbar-corner"></div></div>
+    <!--Sidenav Isquierda-->
